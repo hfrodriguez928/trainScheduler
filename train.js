a87da55cca8a26985b09e5a27ed924dc29d0b24c
hfrodriguez928 <hayleyrodriguez928@gmail.com>
2017-07-09 17:00:32 -0500

    added html layout

---
+++ train.js
@@ -0,0 +1,11 @@
+// Initialize firebase 
+
+var config = {
+    apiKey: "AIzaSyC6oYssVDO7CJJl7TU4zaTr9f2PxJBR4Ko",
+    authDomain: "train-schedule-2f35f.firebaseapp.com",
+    databaseURL: "https://train-schedule-2f35f.firebaseio.com",
+    projectId: "train-schedule-2f35f",
+    storageBucket: "train-schedule-2f35f.appspot.com",
+    messagingSenderId: "992083797256"
+  };
+  firebase.initializeApp(config);Initialize Firebase
